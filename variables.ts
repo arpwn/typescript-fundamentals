55cf4db62c69174ab6b3b6c435c5c3afe3e346f6
Armandeux <armando@MacBook-Air-de-Jesus.local>
2025-12-04 11:18:10 -0600

    feat: agregar ejemplos básicos de variables en TypeScript

---
+++ variables.ts
@@ -0,0 +1,7 @@
+let edad: number = 25;
+let nombre: string = "Armando";
+let esEstudiante: boolean = false;
+let flexible: any = "Puede ser cualquier cosa";
+flexible = 42;
+
+console.log(edad, nombre, esEstudiante, flexible);

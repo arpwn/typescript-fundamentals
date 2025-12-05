870989dbfbb0a8d7fd0aa118761cb2c6071c7e6c
Armandeux <armando@MacBook-Air-de-Jesus.local>
2025-12-05 10:51:45 -0600

    feat: agregar ejemplo basico de types en TypeScript

---
+++ types.ts
@@ -0,0 +1,13 @@
+type Usuario = {
+    nombre: string;
+    edad: number;
+    activo: boolean;
+};
+
+const usuario1: Usuario = {
+    nombre: "Armando",
+    edad: 28,
+    activo: true
+};
+
+console.log(usuario1);

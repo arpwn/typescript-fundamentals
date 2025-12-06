c5132aaaf86233d000cca57a1337dea8d3729297
Armandeux <armando@MacBook-Air-de-Jesus.local>
2025-12-06 13:49:13 -0600

    feat: agregar ejemplos basicos de functions en TypeScript

---
+++ functions.ts
@@ -0,0 +1,10 @@
+function sumar(a: number, b: number): number {
+    return a + b;
+}
+
+function saludar(nombre: string): void {
+    console.log("Hola " + nombre);
+}
+
+console.log(sumar(2, 3));
+saludar("Armando");
